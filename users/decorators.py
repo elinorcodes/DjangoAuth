093db8140bc0141870a486d1d6779804e3a7ab6d
Shubham Anand <shubham_chess@live.com>
2018-05-23 18:11:30 +0530

    add decorators in seperate file

---
+++ users/decorators.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+
+# Custom decorators to handle user behaviour
+# Custom login required decorator
+def login_required(function):
+    def wrapper(request, *args, **kw):
+        user=request.user  
+        if (user.id):
+            return function(request, *args, **kw) 
+        else:
+            return redirect('signup')
+    return wrapper
+
+#Custom logout_required decorator
+def logout_required(function):
+    def wrapper(request, *args, **kw):
+        user=request.user  
+        if not (user.id):
+            return function(request, *args, **kw) 
+        else:
+            return redirect('index')
+    return wrapper
+
+# Custom login required decorator to access group A page
+def login_required_groupA(function):
+    def wrapper(request, *args, **kw):
+        user=request.user  
+        if (user.id and user.profile.group=='A'):
+            return function(request, *args, **kw) 
+        else:
+            return redirect('signup')
+    return wrapper
+
+#Custom logout required decorator to access group B page
+def login_required_groupB(function):
+    def wrapper(request, *args, **kw):
+        user=request.user  
+        if (user.id and user.profile.group=='B'):
+            return function(request, *args, **kw) 
+        else:
+            return redirect('signup')
+    return wrapper
