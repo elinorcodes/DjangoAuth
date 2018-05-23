c9ddd2e1eae438785fde9ceddcabf49e1fd048eb
Shubham Anand <shubham_chess@live.com>
2018-05-23 19:23:01 +0530

    add signIn view

--- users/views.py
+++ users/views.py
@@ -1,11 +1,16 @@
 from django.contrib.auth  import login, authenticate, logout
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 
-from .forms import SignUpForm
+from .forms import SignUpForm, SignInForm
 from .decorators import login_required, login_required_groupA, login_required_groupB, logout_required
 
 # View functions
+
+@logout_required
+def home(request):
+    return render(request, 'users/home.html')
 
 @logout_required
 def signup(request):
@@ -13,6 +18,7 @@ def signup(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
+            print(user)
             user.refresh_from_db()
             user.profile.group = form.cleaned_data.get('group')
             user.save()
@@ -24,6 +30,22 @@ def signup(request):
             print("error")
     form = SignUpForm()
     return render(request, 'users/signup.html', {'form': form})
+
+@logout_required
+def signin(request):
+    if request.method == 'POST':
+        form = SignInForm(request.POST)
+        print(form.is_valid())
+        if form.is_valid():
+            find_user = User.objects.get(username=request.POST['username'])
+            print(find_user.profile.group)
+            user = authenticate(username=find_user.username, password=request.POST['password'])
+            login(request, user)
+            return redirect('index')
+        else:
+            print("error")
+    form = SignInForm()
+    return render(request, 'users/signin.html', {'form': form})
 
 @login_required_groupA
 def groupA(request):
@@ -45,4 +67,4 @@ def index(request):
 @login_required
 def logoutUser(request):
     logout(request)
-    return redirect('signup')
+    return redirect('home')
