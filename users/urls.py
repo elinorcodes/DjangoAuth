392d66fc4286f6f0e43dd212128bcbfd5784b275
Shubham Anand <shubham_chess@live.com>
2018-05-23 19:22:38 +0530

    add signin url

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
     path('logout', views.logoutUser, name='logout'),
     path('groupA', views.groupA, name='groupA'),
     path('groupB', views.groupB, name='groupB'),
-    path('', views.index, name='index')
+    path('index', views.index, name='index')
 ]
