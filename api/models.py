0e30e07e7c5731719b5dfd15e767f3c753438045
Shubham Anand <shubham_chess@live.com>
2018-05-26 20:16:44 +0530

    add Company, Language and Employee Model for api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    languages= models.ManyToManyField(Language)
+    def __str__(self):
+        return self.name
