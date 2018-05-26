e86d83f6a8c9d131e06a52eaae6359fd475ede48
Shubham Anand <shubham_chess@live.com>
2018-05-26 20:22:41 +0530

    edit user model

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    group = models.CharField(max_length=1, blank=True)
+    group = models.CharField(max_length=30, blank=True)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
