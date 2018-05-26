a760778cdb2d931f5611d335ae088d21382fec1e
Shubham Anand <shubham_chess@live.com>
2018-05-27 00:07:01 +0530

    add permissions to views

--- api/views.py
+++ api/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 from .models import Language, Employee, Company
 from .serializers import LanguageSerializer, CompanySerializer, EmployeeSerializer
 from rest_framework import viewsets, permissions
+from .permissions import UserGroupPermissions
 
 # Create your views here.
 class LanguageView(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    permission_classes = (UserGroupPermissions, permissions.IsAuthenticated)
 
 class CompanyView(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+    permission_classes = (UserGroupPermissions, permissions.IsAuthenticated)
 
 class EmployeeView(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    permission_classes = (UserGroupPermissions, permissions.IsAuthenticated)
