7b3e85f848612d4d91a0809dd83c73314919dbea
Shubham Anand <shubham_chess@live.com>
2018-05-26 22:32:01 +0530

    add serializer classes to show json data on website

---
+++ api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import Language, Company, Employee
+
+class LanguageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Language
+        fields = ('id', 'url', 'name')
+        
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('id','url', 'name')
+
+class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ('id', 'url', 'name', 'company' ,'languages')
+
