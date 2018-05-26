ea023c46a3f21a67082531d165df876c4feae72d
Shubham Anand <shubham_chess@live.com>
2018-05-26 22:32:23 +0530

    make view classes

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Language, Employee, Company
+from .serializers import LanguageSerializer, CompanySerializer, EmployeeSerializer
+from rest_framework import viewsets, permissions
 
 # Create your views here.
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+
+class CompanyView(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class EmployeeView(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
